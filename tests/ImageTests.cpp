6f1d8c8e4a5e91ff4040f00916556e41b0b353a0
Jonathan Martin <jonathan.martin@epitech.eu>
2020-04-19 17:50:44 +0200

    added tests on ToolBox and Image

---
+++ tests/ImageTests.cpp
@@ -0,0 +1,17 @@
+//
+// Created by jmartin on 4/19/20.
+//
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <zconf.h>
+#include "../src/Image.hpp"
+#include "../src/Data/Vector2.hpp"
+
+Test(Image, GetSelectedLayerTest)
+{
+    Mimp::Image image{{400, 400}};
+    Mimp::Vector2<unsigned> expected{400, 400};
+
+    cr_assert_eq(image.getSelectedLayer().getSize(), expected);
+}
--- tests/ToolBoxTests.cpp
+++ tests/ToolBoxTests.cpp
@@ -27,3 +27,24 @@ Test(ToolBox, GetWindowTest)
 
     cr_assert_not_null(toolbox.getWindow());
 }
+
+Test(ToolBox, GetSelectedColorTest)
+{
+    tgui::Gui gui{};
+    Mimp::ToolBox toolbox{gui};
+
+    cr_assert_eq(toolbox.getSelectedColor(Mimp::MIMP_LEFT_CLICK), Mimp::Color::Black);
+    cr_assert_eq(toolbox.getSelectedColor(Mimp::MIMP_RIGHT_CLICK), Mimp::Color::White);
+}
+
+Test(ToolBox, SetSelectedColorTest)
+{
+    tgui::Gui gui{};
+    Mimp::ToolBox toolbox{gui};
+
+    toolbox.setSelectedColor(Mimp::MIMP_LEFT_CLICK, Mimp::Color::Red);
+    toolbox.setSelectedColor(Mimp::MIMP_RIGHT_CLICK, Mimp::Color::Cyan);
+
+    cr_assert_eq(toolbox.getSelectedColor(Mimp::MIMP_LEFT_CLICK), Mimp::Color::Red);
+    cr_assert_eq(toolbox.getSelectedColor(Mimp::MIMP_RIGHT_CLICK), Mimp::Color::Cyan);
+}
