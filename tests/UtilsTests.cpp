0a40e8c08fe6e49f15d1cfe3e63eb60d440d82d4
Jonathan Martin <jonathan.martin@epitech.eu>
2020-04-22 14:50:04 +0200

    added UT on GetLastExceptionName

--- tests/UtilsTests.cpp
+++ tests/UtilsTests.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <zconf.h>
 #include "../src/Utils.hpp"
+#include "../src/Exceptions.hpp"
 
 Test(Utils, OpenAndSaveFileDialogTest)
 {
@@ -23,6 +24,56 @@ Test(Utils, GetLastExceptionTest)
 	std::string lastException = Mimp::Utils::getLastExceptionName();
 
 	cr_assert_eq(lastException, "No exception");
+}
+
+Test(Utils, GetLastExceptionTest2)
+{
+	try {
+		throw Mimp::CorruptedGuiFileException("File Corrupted");
+	} catch (Mimp::BaseException &e) {
+		std::string lastException = Mimp::Utils::getLastExceptionName();
+		cr_assert_eq(lastException, "Mimp::CorruptedGuiFileException");
+	}
+}
+
+Test(Utils, GetLastExceptionTest3)
+{
+	try {
+		throw Mimp::InvalidArgumentException("InvalidArgument");
+	} catch (Mimp::BaseException &e) {
+		std::string lastException = Mimp::Utils::getLastExceptionName();
+		cr_assert_eq(lastException, "Mimp::InvalidArgumentException");
+	}
+}
+
+Test(Utils, GetLastExceptionTest4)
+{
+	try {
+		throw Mimp::OutOfBoundException("Out of Bounds");
+	} catch (Mimp::BaseException &e) {
+		std::string lastException = Mimp::Utils::getLastExceptionName();
+		cr_assert_eq(lastException, "Mimp::OutOfBoundException");
+	}
+}
+
+Test(Utils, GetLastExceptionTest5)
+{
+	try {
+		throw Mimp::InvalidImageException("InvalidImageException");
+	} catch (Mimp::BaseException &e) {
+		std::string lastException = Mimp::Utils::getLastExceptionName();
+		cr_assert_eq(lastException, "Mimp::InvalidImageException");
+	}
+}
+
+Test(Utils, GetLastExceptionTest6)
+{
+	try {
+		throw Mimp::ExportErrorException("ExportErrorException");
+	} catch (Mimp::BaseException &e) {
+		std::string lastException = Mimp::Utils::getLastExceptionName();
+		cr_assert_eq(lastException, "Mimp::ExportErrorException");
+	}
 }
 
 Test(Utils, PathToStringTest)
