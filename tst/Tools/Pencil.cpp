5f2b3faa943bd94faeb309308a82e9ae4b1fd76b
Thomas Bouvier <thomas.bouvier@epitech.eu>
2020-05-15 22:09:51 +0200

    Add simple Eraser and Pencil tests.

--- tst/Tools/Pencil.cpp
+++ tst/Tools/Pencil.cpp
@@ -17,5 +17,128 @@ TEST(Pencil, getDefaultPanelValues) {
     ASSERT_TRUE(radiusPreview->getText() == "5");
 }
 
-// onMouseDrag
-// onClick
+TEST(Pencil, onClick) {
+    tgui::Gui gui{};
+    Mimp::ToolBox toolbox{gui};
+    Mimp::Pencil pencil{toolbox};
+    Mimp::Vector2<unsigned> size{10, 10};
+    Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
+    Mimp::Image image(size, lm);
+    Mimp::Vector2<int> erasePos = {2, 2};
+    float radius = pencil.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
+
+    ASSERT_EQ(image.getLayers().size(), 1);
+    auto buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+        }
+    }
+
+    pencil.onClick(erasePos, Mimp::MIMP_LEFT_CLICK, image);
+
+    buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            if (i >= erasePos.x - 1 && i < erasePos.x + radius - 2
+                && j >= erasePos.y - 1 && j < erasePos.y + radius - 2) {
+                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Black);
+            } else {
+                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+            }
+        }
+    }
+}
+
+TEST(Pencil, onDrag) {
+    tgui::Gui gui{};
+    Mimp::ToolBox toolbox{gui};
+    Mimp::Pencil pencil{toolbox};
+    Mimp::Vector2<unsigned> size{10, 10};
+    Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
+    Mimp::Image image(size, lm);
+    Mimp::Vector2<int> erasePosStart = {2, 2};
+    Mimp::Vector2<int> erasePosEnd = {8, 2};
+    float radius = pencil.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
+
+    ASSERT_EQ(image.getLayers().size(), 1);
+    auto buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+        }
+    }
+
+    pencil.onMouseDrag(erasePosStart, erasePosEnd, Mimp::MIMP_LEFT_CLICK, image);
+
+    buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            if (i >= erasePosStart.x - 1 && i < erasePosEnd.x + radius - 2
+                && j >= erasePosStart.y - 1 && j < erasePosEnd.y + radius - 2) {
+                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Black);
+            } else {
+                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+            }
+        }
+    }
+}
+
+TEST(Pencil, onClickWithLockedLayer) {
+    tgui::Gui gui{};
+    Mimp::ToolBox toolbox{gui};
+    Mimp::Pencil pencil{toolbox};
+    Mimp::Vector2<unsigned> size{10, 10};
+    Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
+    Mimp::Image image(size, lm);
+    Mimp::Vector2<int> erasePos = {2, 2};
+    float radius = pencil.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
+
+    ASSERT_EQ(image.getLayers().size(), 1);
+    auto buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+        }
+    }
+
+    image.getLayers()[0].locked = true;
+    pencil.onClick(erasePos, Mimp::MIMP_LEFT_CLICK, image);
+
+    buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+        }
+    }
+}
+
+TEST(Pencil, onDragWithLockedLayer) {
+    tgui::Gui gui{};
+    Mimp::ToolBox toolbox{gui};
+    Mimp::Pencil pencil{toolbox};
+    Mimp::Vector2<unsigned> size{10, 10};
+    Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
+    Mimp::Image image(size, lm);
+    Mimp::Vector2<int> erasePosStart = {2, 2};
+    Mimp::Vector2<int> erasePosEnd = {8, 2};
+    float radius = pencil.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
+
+    ASSERT_EQ(image.getLayers().size(), 1);
+    auto buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+        }
+    }
+
+    image.getLayers()[0].locked = true;
+    pencil.onMouseDrag(erasePosStart, erasePosEnd, Mimp::MIMP_LEFT_CLICK, image);
+
+    buffer = image.getLayers()[0].buffer.getBuffer();
+    for (int i = 0; i < size.x; i += 1) {
+        for (int j = 0; j < size.y; j += 1) {
+            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+        }
+    }
+}
