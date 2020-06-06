48d0de41432380d7727072c21af5355f1e5648fa
Thomas Bouvier <thomas.bouvier@epitech.eu>
2020-06-06 20:22:54 +0200

    GIMP-like light effect.

--- src/ImageOperations/Effects/Effects.cpp
+++ src/ImageOperations/Effects/Effects.cpp
@@ -15,9 +15,6 @@ namespace Mimp {
 
 	void Effects::click(tgui::Gui &gui, CanvasWidget::Ptr image, tgui::ChildWindow::Ptr window, Editor &editor) const
 	{
-		//! @todo Image Manipulation
-		image->takeFrameBufferSnapshot();
-
 		auto lightSlider = this->_win->get<tgui::Slider>("Light");
 		auto lightPreview = this->_win->get<tgui::TextBox>("LightPreview");
 
@@ -26,17 +23,29 @@ namespace Mimp {
 
 		});
 		lightSlider->connect("MouseLeft", [lightSlider, image] {
+			image->takeFrameBufferSnapshot();
+
 			auto buffer = image->getSelectedLayer().buffer;
 			auto size = buffer->getSize();
+			auto value = lightSlider->getValue();
 
 			for (unsigned int x = 0; x < size.x; x += 1) {
 				for (unsigned int y = 0; y < size.y; y += 1) {
 					auto newColor = buffer->getPixel(Vector2<int>(x, y));
 
-					newColor.a = lightSlider->getValue();
+					if (newColor.r + value <= 0) newColor.r = 0;
+					else if (newColor.r + value >= 255) newColor.r = 255;
+					else newColor.r += value;
+					if (newColor.g + value <= 0) newColor.g = 0;
+					else if (newColor.g + value >= 255) newColor.g = 255;
+					else newColor.g += value;
+					if (newColor.b + value <= 0) newColor.b = 0;
+					else if (newColor.b + value >= 255) newColor.b = 255;
+					else newColor.b += value;
 					buffer->setPixel(Vector2<int>(x, y), newColor);
 				}
 			}
+			lightSlider->setValue(0);
 		});
 
 		this->_win->connect("Closed", [this, &gui]{
