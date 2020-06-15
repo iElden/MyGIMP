f0cb805785b3455c950e545ae2e4c3ca6e6702c8
Gegel85 <Gegel85@laposte.net>
2020-06-16 01:25:54 +0200

    Fixed bug causing Lasso to not reset properly

--- src/Tools/Lasso.cpp
+++ src/Tools/Lasso.cpp
@@ -9,30 +9,23 @@ namespace Mimp {
 		this->setKeyCombination({Keys::KEY_L, false, false, false});
 	}
 
-	void Lasso::onMouseDrag(Vector2<int>, Vector2<int> newPos, MouseClick click, Image &image)
+	void Lasso::onMouseDrag(Vector2<int>, Vector2<int> newPos, MouseClick click, Image &)
 	{
-		if (click == MouseClick::MIMP_LEFT_CLICK) {
+		if (click == MouseClick::MIMP_LEFT_CLICK)
 			this->_polygon.add(newPos);
-		}
 	}
 
 	void Lasso::onClick(Vector2<int>, MouseClick click, Image &image)
 	{
-		image.takeSelectionSnapshot();
-		image.selectedArea->clear();
+		if (click == MouseClick::MIMP_LEFT_CLICK) {
+			image.selectedArea->clear();
+			this->_polygon.clear();
+		}
 	}
 
 	tgui::ScrollablePanel::Ptr Lasso::getParametersPanel()
 	{
-		auto panel = tgui::ScrollablePanel::create();
-		panel->loadWidgetsFromFile("widgets/tools_cfg/lasso_cfg.gui");
-
-		auto button = panel->get<tgui::Button>("Clear");
-
-		button->connect("Pressed", [this] { this->clear(); });
-		panel->add(button);
-
-		return panel;
+		return tgui::ScrollablePanel::create();
 	}
 
 	void Lasso::clear()
@@ -40,10 +33,12 @@ namespace Mimp {
 		this->_polygon.reset();
 	}
 
-	void Lasso::onMouseRelease(Vector2<int> vector2, MouseClick click, Image &image)
+	void Lasso::onMouseRelease(Vector2<int>, MouseClick click, Image &image)
 	{
-		image.takeSelectionSnapshot();
-		image.selectedArea->clear();
-		this->_polygon.update([&image](Vector2<int> pt) { image.selectedArea->add(pt); });
+		if (click == MouseClick::MIMP_LEFT_CLICK) {
+			image.takeSelectionSnapshot();
+			image.selectedArea->clear();
+			this->_polygon.update([&image](Vector2<int> pt) { image.selectedArea->add(pt); });
+		}
 	}
 }
