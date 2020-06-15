407222ec0b8e24f40ced3cc8bfbc419b0f378d10
iElden <mceldenn@gmail.com>
2020-06-15 21:20:15 +0200

    Clean indent inconsistence

--- tst/Tools/Eraser.cpp
+++ tst/Tools/Eraser.cpp
@@ -5,84 +5,83 @@
 #define INT(x) (static_cast<int>(x))
 
 TEST(Eraser, getDefaultPanelValues) {
-    tgui::Gui gui{};
-    Mimp::ToolBox toolbox{gui};
-    Mimp::Eraser eraser{toolbox};
+	tgui::Gui gui{};
+	Mimp::ToolBox toolbox{gui};
+	Mimp::Eraser eraser{toolbox};
+	auto panel = eraser.getParametersPanel();
+	auto radiusSlider = panel->get<tgui::Slider>("Radius");
+	auto shapeBox = panel->get<tgui::ComboBox>("Shape");
+	auto radiusPreview = panel->get<tgui::TextBox>("RadiusPreview");
 
-    auto panel = eraser.getParametersPanel();
-    auto radiusSlider = panel->get<tgui::Slider>("Radius");
-    auto shapeBox = panel->get<tgui::ComboBox>("Shape");
-    auto radiusPreview = panel->get<tgui::TextBox>("RadiusPreview");
-
-    ASSERT_TRUE(radiusSlider->getValue() == 5.0);
-    ASSERT_TRUE(shapeBox->getSelectedItemIndex() == Mimp::DrawShape::CIRCLE);
-    ASSERT_TRUE(radiusPreview->getText() == "5");
+	ASSERT_TRUE(radiusSlider->getValue() == 5.0);
+	ASSERT_TRUE(shapeBox->getSelectedItemIndex() == Mimp::DrawShape::CIRCLE);
+	ASSERT_TRUE(radiusPreview->getText() == "5");
 }
 
 TEST(Eraser, onClick) {
-    tgui::Gui gui{};
-    Mimp::ToolBox toolbox{gui};
-    Mimp::Eraser eraser{toolbox};
-    Mimp::Vector2<unsigned> size{10, 10};
-    Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
-    Mimp::Image image(size, lm);
-    Mimp::Vector2<int> erasePos = {2, 2};
-    float radius = eraser.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
+	tgui::Gui gui{};
+	Mimp::ToolBox toolbox{gui};
+	Mimp::Eraser eraser{toolbox};
+	Mimp::Vector2<unsigned> size{10, 10};
+	Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
+	Mimp::Image image(size, lm);
+	Mimp::Vector2<int> erasePos = {2, 2};
+	float radius = eraser.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
 
-    ASSERT_EQ(image.getLayers().size(), 1);
-    auto buffer = image.getLayers()[0]->buffer.getBuffer();
-    for (int i = 0; i < INT(size.x); i += 1) {
-        for (int j = 0; j < INT(size.y); j += 1) {
-            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
-        }
-    }
+	ASSERT_EQ(image.getLayers().size(), 1);
+	auto buffer = image.getLayers()[0]->buffer.getBuffer();
+	for (int i = 0; i < INT(size.x); i += 1) {
+		for (int j = 0; j < INT(size.y); j += 1) {
+			ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+		}
+	}
 
-    eraser.onClick(erasePos, Mimp::MIMP_LEFT_CLICK, image);
+	eraser.onClick(erasePos, Mimp::MIMP_LEFT_CLICK, image);
 
-    buffer = image.getLayers()[0]->buffer.getBuffer();
-    for (int i = 0; i < INT(size.x); i += 1) {
-        for (int j = 0; j < INT(size.y); j += 1) {
-            if (i >= erasePos.x - 1 && i < erasePos.x + INT(radius) - 2
-            && j >= erasePos.y - 1 && j < erasePos.y + INT(radius) - 2) {
-                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Transparent);
-            } else {
-                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
-            }
-        }
-    }
+	buffer = image.getLayers()[0]->buffer.getBuffer();
+	for (int i = 0; i < INT(size.x); i += 1) {
+		for (int j = 0; j < INT(size.y); j += 1) {
+			if (i >= erasePos.x - 1 && i < erasePos.x + INT(radius) - 2
+			&& j >= erasePos.y - 1 && j < erasePos.y + INT(radius) - 2) {
+				ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Transparent);
+			} else {
+				ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+			}
+		}
+	}
 }
 
 TEST(Eraser, onDrag) {
-    tgui::Gui gui{};
-    Mimp::ToolBox toolbox{gui};
-    Mimp::Eraser eraser{toolbox};
-    Mimp::Vector2<unsigned> size{10, 10};
-    Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
-    Mimp::Image image(size, lm);
-    Mimp::Vector2<int> erasePosStart = {2, 2};
-    Mimp::Vector2<int> erasePosEnd = {8, 2};
-    float radius = eraser.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
+	tgui::Gui gui{};
+	Mimp::ToolBox toolbox{gui};
+	Mimp::Eraser eraser{toolbox};
+	Mimp::Vector2<unsigned> size{10, 10};
+	Mimp::LayerManager lm(size, 1, Mimp::Color::Magenta);
+	Mimp::Image image(size, lm);
+	Mimp::Vector2<int> erasePosStart = {2, 2};
+	Mimp::Vector2<int> erasePosEnd = {8, 2};
+	float radius = eraser.getParametersPanel()->get<tgui::Slider>("Radius")->getValue();
 
-    ASSERT_EQ(image.getLayers().size(), 1);
-    auto buffer = image.getLayers()[0]->buffer.getBuffer();
-    for (int i = 0; i < INT(size.x); i += 1) {
-        for (int j = 0; j < INT(size.y); j += 1) {
-            ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
-        }
-    }
+	ASSERT_EQ(image.getLayers().size(), 1);
+	auto buffer = image.getLayers()[0]->buffer.getBuffer();
+	for (int i = 0; i < INT(size.x); i += 1) {
+		for (int j = 0; j < INT(size.y); j += 1) {
+			ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+		}
+	}
 
-    eraser.onMouseDrag(erasePosStart, erasePosEnd, Mimp::MIMP_LEFT_CLICK, image);
+	eraser.onMouseDrag(erasePosStart, erasePosEnd, Mimp::MIMP_LEFT_CLICK, image);
 
-    buffer = image.getLayers()[0]->buffer.getBuffer();
-    for (int i = 0; i < INT(size.x); i += 1) {
-        for (int j = 0; j < INT(size.y); j += 1) {
-            if (i >= erasePosStart.x - 1 && i < erasePosEnd.x + INT(radius) - 2
-                && j >= erasePosStart.y - 1 && j < erasePosEnd.y + INT(radius) - 2) {
-                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Transparent);
-            } else {
-                ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
-            }
-        }
+	buffer = image.getLayers()[0]->buffer.getBuffer();
+	for (int i = 0; i < INT(size.x); i += 1) {
+		for (int j = 0; j < INT(size.y); j += 1) {
+			if (i >= erasePosStart.x - 1 && i < erasePosEnd.x + INT(radius) - 2
+				&& j >= erasePosStart.y - 1 && j < erasePosEnd.y + INT(radius) - 2) {
+				ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Transparent);
+			} else {
+				ASSERT_TRUE(buffer[i + size.x * j] == Mimp::Color::Magenta);
+			}
+	    }
     }
 }
 
